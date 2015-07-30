e7713ea4d59eb8448f014aa5250d40bc80bf7460
Joshua Steverman <jstever@punch.umdl.umich.edu>
2015-07-30 11:14:16 -0400

    moving the phone.png locally to prevent https errors.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+#     ^ thats a lie
 Rails.application.config.assets.precompile += %w( cancel.png )
 Rails.application.config.assets.precompile += %w( gdr_edits.css )
