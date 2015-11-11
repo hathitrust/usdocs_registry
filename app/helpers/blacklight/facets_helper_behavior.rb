0331e0cc089961d677b1ebc9c8d2aee478b8425c
Joshua Steverman <jstever@punch.umdl.umich.edu>
2015-11-11 13:55:28 -0500

    nofollow facet links

--- app/helpers/blacklight/facets_helper_behavior.rb
+++ app/helpers/blacklight/facets_helper_behavior.rb
@@ -128,7 +128,7 @@ module Blacklight::FacetsHelperBehavior
   def render_facet_value(facet_field, item, options ={})
     path = search_action_path(add_facet_params_and_redirect(facet_field, item))
     content_tag(:span, :class => "facet-label") do
-      link_to_unless(options[:suppress_link], facet_display_value(facet_field, item), path, :class=>"facet_select")
+      link_to_unless(options[:suppress_link], facet_display_value(facet_field, item), path, :class=>"facet_select", :rel=>"nofollow")
     end + render_facet_count(item.hits)
   end
 
